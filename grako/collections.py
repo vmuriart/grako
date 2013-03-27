17fcd81e489b33cf8eb9d8b316f04d7c3ff50aab
Juancarlo A?ez <juancarlo.anez@gmail.com>
2013-03-27 13:18:52 -0530

    Rename counts to 'life', and increase the life of recovered cache items by the threshold.

--- grako/collections.py
+++ grako/collections.py
@@ -6,24 +6,24 @@ import collections
 class CountBoundedDict(collections.OrderedDict):
     def __init__(self, *args, **kwargs):
         self.threshold = kwargs.pop('threshold', 7)
-        self.counts = collections.Counter()
+        self.life = collections.Counter()
         super(CountBoundedDict, self).__init__(*args, **kwargs)
 
     def __getitem__(self, key):
-        self.counts[key] += 1
+        self.life[key] += self.threshold
         value = super(CountBoundedDict, self).__getitem__(key)
         super(CountBoundedDict, self).__setitem__(key, value)  # move it to last
         return value
 
     def __setitem__(self, key, value):
         super(CountBoundedDict, self).__setitem__(key, value)
-        self.counts[key] += self.threshold
+        self.life[key] += self.threshold
         t = -self.threshold
         for key in self:
-            value = self.counts[key] - 1
+            value = self.life[key] - 1
             if value < t:
                 del self[key]
-                del self.counts[key]
+                del self.life[key]
                 return  # delete only one!
             else:
-                self.counts[key] = value
+                self.life[key] = value
