e197743cb36448ca6da1aeb3b53bcd5493cfe4ac
Juancarlo A?ez <juancarlo.anez@gmail.com>
2013-08-08 12:57:22 -0530

    No change.

---
+++ grako/test/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+The Grako test suite.
+"""
+from __future__ import print_function, division, absolute_import, unicode_literals
+
+if __name__ == '__main__':
+    from . import bootstrap_tests
+    from . import  buffering_test
+    from . import  grammar_tests
+
+    bootstrap_tests.main()
+    buffering_test.main()
+    grammar_tests.main()
