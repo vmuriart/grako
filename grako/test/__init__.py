b5dee7b8decc5ed171f5653bca98b84e833b9bb7
Juancarlo A?ez <juancarlo.anez@gmail.com>
2014-05-22 14:17:18 -0530

    Generic support for includes from upstream.

---
+++ grako/test/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+The Grako test suite.
+"""
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+if __name__ == '__main__':
+    import unittest
+    from . import bootstrap_test
+    from . import buffering_test
+    from . import grammar_test
+    from . import codegen_test
+
+    suite = unittest.TestSuite(tests=[
+        bootstrap_test.suite(),
+        buffering_test.suite(),
+        grammar_test.suite(),
+        codegen_test.suite()
+    ])
+
+    unittest.TextTestRunner(verbosity=2).run(suite)
