7fa0309fb5369c466880a106a9e74a26719e0bc3
Juancarlo A?ez <juancarlo.anez@gmail.com>
2014-05-22 15:57:40 -0530

    More support for includes in Buffer, and support for a ..include:: pragma in Grako grammars.

---
+++ grako/ast.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Define the AST class, a direct descendant of dict that's used during parsing
+to store the values of named elements of grammar rules.
+"""
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+__all__ = ['AST']
+
+
+class AST(dict):
+    """
+    A dictionary with attribute-style access. It maps attribute access to
+    the real dictionary.
+    """
+    # ActiveState Recipe:
+    # http://code.activestate.com/recipes/473786-dictionary-with-attribute-style-access/
+
+    def __getstate__(self):
+        return self.__dict__.items()
+
+    def __setstate__(self, items):
+        for key, val in items:
+            self.__dict__[key] = val
+
+    def __repr__(self):
+        return "%s(%s)" % (self.__class__.__name__, super(AST, self).__repr__())
+
+    def __setitem__(self, key, value):
+        self._add(key, value)
+
+    def __getitem__(self, key):
+        if self.__contains__(key):
+            return super(AST, self).__getitem__(key)
+
+    __getattr__ = __getitem__
+    __setattr__ = __setitem__
+
+    def __getattribute__(self, name):
+        if name in self:
+            return self[name]
+        return super(AST, self).__getattribute__(name)
+
+    def _define(self, keys):
+        for key in keys:
+            if not self.__contains__(key):
+                super(AST, self).__setitem__(key, None)
+
+    def _copy(self):
+        haslists = any(isinstance(v, list) for v in self.values())
+        if not haslists:
+            return AST(self)
+        return AST(
+            (k, v[:] if isinstance(v, list) else v)
+            for k, v in self.items()
+        )
+
+    def _add(self, key, value, force_list=False):
+        previous = self.get(key, None)
+        if previous is None:
+            if force_list:
+                super(AST, self).__setitem__(key, [value])
+            else:
+                super(AST, self).__setitem__(key, value)
+        elif isinstance(previous, list):
+            previous.append(value)
+        else:
+            super(AST, self).__setitem__(key, [previous, value])
+        return self
+
+    def _append(self, key, value):
+        return self._add(key, value, force_list=True)
+
+    @property
+    def parseinfo(self):
+        """ Make the special attribute `_parseinfo` be available
+            as a property without an underscore in the name.
+            This patch helps with backwards compatibility.
+        """
+        return self._parseinfo
---
+++ grako/parser.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Implements parsing of Grako's EBNF idiom for grammars, and grammar model
+creation using the .grammars module.
+
+GrakoParserRoot is the bootstrap parser. It uses the facilities of parsing.Parser
+as generated parsers do, but it does not conform to the patterns in the generated
+code. Why? Because having Grako bootstrap itself from its grammar would be cool,
+but very bad engineering. GrakoParserRoot is hand-crafted.
+
+The GrakoGrammarGenerator class, a descendant of GrakoParserRoot constructs
+a model of the grammar using semantic actions the model elements defined
+in the .grammars module.
+"""
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+from .exceptions import ParseError
+from .buffering import Buffer
+from .semantics import GrakoASTSemantics, GrakoSemantics
+from .bootstrap import GrakoBootstrapParser
+
+__all__ = ['GrakoParser', 'GrakoGrammarGenerator']
+
+COMMENTS_RE = r'\(\*(?:.|\n)*?\*\)'
+
+
+class GrakoBuffer(Buffer):
+    def process_block(self, name, lines, index, **kwargs):
+        i = 0
+        while i < len(lines):
+            line = lines[i]
+            if line.startswith('..'):
+                name, arg = line.split('..')[1], ''
+                if '::' in name:
+                    name, arg = name.split('::')
+                name, arg = name.strip(), arg.strip()
+                i = self.pragma(name, arg, lines, index, i)
+            else:
+                i += 1
+        return lines, index
+
+    def pragma(self, name, arg, lines, index, i):
+        if name == 'include':
+            return self.include_file(arg, lines, index, i, i)
+        else:
+            raise ParseError('Unknown pragma: %s' % name)
+
+
+class GrakoParserBase(GrakoBootstrapParser):
+
+    def parse(self, text, rule='grammar', filename=None, **kwargs):
+        if not isinstance(text, Buffer):
+            text = GrakoBuffer(text, comments_re=COMMENTS_RE, **kwargs)
+        return super(GrakoParserBase, self).parse(
+            text,
+            rule,
+            filename=filename,
+            **kwargs
+        )
+
+
+class GrakoParser(GrakoParserBase):
+    def __init__(self, grammar_name, semantics=None, **kwargs):
+        if semantics is None:
+            semantics = GrakoASTSemantics()
+        super(GrakoParser, self).__init__(semantics=semantics, **kwargs)
+
+
+class GrakoGrammarGenerator(GrakoParserBase):
+    def __init__(self, grammar_name, semantics=None, **kwargs):
+        if semantics is None:
+            semantics = GrakoSemantics(grammar_name)
+        super(GrakoGrammarGenerator, self).__init__(semantics=semantics, **kwargs)
