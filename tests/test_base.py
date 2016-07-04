c614165d5f15c61198d25674b21304a8d5307939
Victor Uriarte <victor.m.uriarte@intel.com>
2016-07-04 16:33:36 -0700

    unicodeeeee

--- tests/test_base.py
+++ tests/test_base.py
@@ -8,4 +8,3 @@ def test_basic():
     parser = parser_base.UnknownParser(parseinfo=True)
     ast = parser.parse('select 1 from dual', rule_name='start')
     assert isinstance(ast, AST)
-    assert str(ast) == "AST({u'select': [u'SELECT', [u'1'], [u'FROM', [u'dual']]]})"
