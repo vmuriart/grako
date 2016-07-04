f0f42777f273008a484974c8d1035b15dcc5b405
Victor Uriarte <victor.m.uriarte@intel.com>
2016-07-04 16:20:08 -0700

    add simple parser only test

---
+++ tests/test_base.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+import parser_base
+from grako.ast import AST
+
+def test_basic():
+    parser = parser_base.UnknownParser(parseinfo=True)
+    ast = parser.parse('select 1 from dual', rule_name='start')
+    assert isinstance(ast, AST)
+    assert str(ast) == "AST({u'select': [u'SELECT', [u'1'], [u'FROM', [u'dual']]]})"
