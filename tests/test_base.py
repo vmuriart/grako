dd61fc3a03d85cb69ef943e1dd520c7759912bed
Victor Uriarte <victor.m.uriarte@intel.com>
2016-07-04 19:01:32 -0700

    change result output

--- tests/test_base.py
+++ tests/test_base.py
@@ -10,7 +10,7 @@ def test_basic():
     ast = parser.parse("""-- comment
               SELECT 1 a, 2 b, 3 c, d FROM dual t, triple WHERE 1 = 4
             """, rule_name='start')
-    assert isinstance(ast, AST)
+    assert isinstance(ast, list)
 
 
 def test_fail():
