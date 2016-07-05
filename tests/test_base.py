e3642ab441d595fd5fc3ec26abaf3e18a63e5278
Victor Uriarte <victor.m.uriarte@intel.com>
2016-07-04 17:15:17 -0700

    add more complex test for better coverage testing

--- tests/test_base.py
+++ tests/test_base.py
@@ -3,8 +3,20 @@
 import pytest
 from . import parser_base
 from grako.ast import AST
+from grako.exceptions import FailedParse
 
 def test_basic():
-    parser = parser_base.UnknownParser(parseinfo=True)
-    ast = parser.parse('select 1 from dual', rule_name='start')
+    parser = parser_base.SqlParser(parseinfo=True)
+    ast = parser.parse("""-- comment
+              SELECT 1 a, 2 b, 3 c, d FROM dual t, triple WHERE 1 = 4
+            """, rule_name='start')
     assert isinstance(ast, AST)
+
+
+def test_fail():
+    parser = parser_base.SqlParser(parseinfo=True)
+    with pytest.raises(FailedParse):
+        ast = parser.parse("""-- comment
+              SELECT 1 a, 2 b, 3 c, d FROM dual t, triple WHERE
+            """, rule_name='start')
+
