183c762f6548bc2f9b8f4f4f2893d5dcf98d5c12
Victor Uriarte <victor.m.uriarte@intel.com>
2016-07-04 20:39:51 -0700

    simplify parse call

--- tests/test_base.py
+++ tests/test_base.py
@@ -4,11 +4,11 @@ import pytest
 from . import parser_base
 from grako.exceptions import FailedParse
 
+
 def test_basic():
     parser = parser_base.SqlParser()
     ast = parser.parse("""-- comment
-              SELECT 1 a, 2 b, 3 c, d FROM dual t, triple WHERE 1 = 4
-            """, rule_name='start')
+              SELECT 1 a, 2 b, 3 c, d FROM dual t, triple WHERE 1 = 4""")
     assert isinstance(ast, list)
 
 
@@ -16,6 +16,4 @@ def test_fail():
     parser = parser_base.SqlParser()
     with pytest.raises(FailedParse):
         parser.parse("""-- comment
-              SELECT 1 a, 2 b, 3 c, d FROM dual t, triple WHERE
-            """, rule_name='start')
-
+              SELECT 1 a, 2 b, 3 c, d FROM dual t, triple WHERE""")
